73d49c8c4dd621e1418eb0fe2d8d824d9edc1745
annTerry <annak@1gb.ru>
2023-01-09 16:23:25 +0300

    feat: task resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+  let reverseN = Math.abs(n).toString().split("").reverse();
+  while ( reverseN.length > 1 && reverseN[0] === "0") {
+    reverseN.shift();
+  }
+  const result = reverseN.join("");
+  return +result;
 }
